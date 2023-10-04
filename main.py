806fba625c08a5c57f20e88d373cc8464970a954
Yabbanootje <k.a.b.toonen@student.tue.nl>
2023-10-04 16:04:56 +0200

    Optimzer
    Co-Authored-By: CoenSchouten01 <64014994+CoenSchouten01@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torchsummary import summary
-from torchvision.datasets import CIFAR10
+from torchvision.datasets import CIFAR10, MNIST
 import torchvision
 import torch.optim as optim
 import matplotlib.pyplot as plt
@@ -16,7 +16,7 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # print(summary(model, (3, 244, 244)))
 
 class LinearModel(nn.Module):
-    def __init__(self, input_size=3*32*32, n_classes=2):
+    def __init__(self, input_size=1*28*28, n_classes=2):
         super().__init__()
         self.flatten = nn.Flatten()
         self.layer = nn.Linear(input_size, n_classes)
@@ -25,13 +25,33 @@ class LinearModel(nn.Module):
         return self.layer(self.flatten(x))
         
 
-def probability(counts):
-    # counts = np.zeros((n_classes, n_classes, n_classes))
-    # F_pred = np.argmax(F)
-    # L_pred = np.argmax(L)
-
-    probs = counts/np.sum(counts)
-    return
+def mutual_information(probs, conditioning=False):
+    # Calculate I()
+    n_classes = probs.shape[0]
+    mi = 0
+    
+    joint_ly = np.sum(probs, axis=0)
+    joint_fy = np.sum(probs, axis=1)
+    joint_fl = np.sum(probs, axis=2)
+    
+    for f in range(n_classes):
+        p_f = np.sum(joint_fy, axis=1)[f]
+        for y in range(n_classes):
+            p_fy = joint_fy[f, y]
+            p_y = np.sum(joint_fy, axis=0)[y]
+            if conditioning == False:
+                mi += p_fy * np.log2(p_fy / (p_f * p_y))
+            else:
+                for l in range(n_classes):
+                    p_fly = probs[f, l, y]
+                    p_fl = joint_fl[f, l]
+                    p_ly = joint_ly[l, y]
+                    p_l = np.sum(joint_fl, axis=0)[l]
+                    p_fy_given_l = p_fly / p_l
+                    p_f_given_l = p_fl / p_l
+                    p_y_given_l = p_ly / p_l
+                    mi += p_fly * np.log2(p_fy_given_l / (p_f_given_l * p_y_given_l))
+    return mi
 
 def class_transform(label):
     return int(label > 4)
@@ -39,11 +59,13 @@ def class_transform(label):
 def get_data(batch_size):
     transform = transforms.ToTensor()
     
-    trainset = CIFAR10(root='./data', train=True, download=True, transform=transform, target_transform=class_transform)
+    trainset = MNIST(root='./data', train=True, download=True, transform=transform, target_transform=class_transform)
+    # trainset = CIFAR10(root='./data', train=True, download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
 
-    testset = CIFAR10(root='./data', train=False, download=True, transform=transform, target_transform=class_transform)
+    testset = MNIST(root='./data', train=False, download=True, transform=transform, target_transform=class_transform)
+    # testset = CIFAR10(root='./data', train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=False, num_workers=2)
     
@@ -69,28 +91,31 @@ def batch_loss(model, loss_func, xb, yb, optimizer = None):
 
     return loss.item(), len(xb)
 
-def fit(conv_model, linear_model, epochs, loss_func, trainloader, testloader, optimizer, n_classes=2):
+def fit(conv_model, linear_model, epochs, loss_func, trainloader, testloader, conv_optimizer, linear_optimizer, n_classes=2):
     
     for epoch in tqdm(range(epochs)):
         conv_model.train()
         linear_model.train()
         running_loss_conv = 0
         sample_nums = 0
+        history_mi_train = []
+        history_mi_test = []
+        history_mu = []
 
-        # for xb, yb in trainloader:
-        #     xb = xb.to(DEVICE)
-        #     yb = yb.to(DEVICE)
-        #     loss_batch_conv, nums = batch_loss(conv_model, loss_func, xb, yb, optimizer)
-        #     loss_batch_linear, _ = batch_loss(linear_model, loss_func, xb, yb, optimizer)
+        for xb, yb in trainloader:
+            xb = xb.to(DEVICE)
+            yb = yb.to(DEVICE)
+            loss_batch_conv, nums = batch_loss(conv_model, loss_func, xb, yb, conv_optimizer)
+            loss_batch_linear, _ = batch_loss(linear_model, loss_func, xb, yb, linear_optimizer)
 
-        #     running_loss_conv += loss_batch_conv * xb.size(0)
-        #     sample_nums += nums
-        #     loss_conv = running_loss_conv / sample_nums
-        # print(
-        #     f'EPOCH: {epoch+1:0>{len(str(epochs))}}/{epochs}',
-        #     end=' '
-        # )
-        # print(f'LOSS: {loss_conv:.4f}',end=' ')
+            running_loss_conv += loss_batch_conv * xb.size(0)
+            sample_nums += nums
+            loss_conv = running_loss_conv / sample_nums
+        print(
+            f'EPOCH: {epoch+1:0>{len(str(epochs))}}/{epochs}',
+            end=' '
+        )
+        print(f'LOSS: {loss_conv:.4f}',end=' ')
 
         
         conv_model.eval()
@@ -98,29 +123,60 @@ def fit(conv_model, linear_model, epochs, loss_func, trainloader, testloader, op
 
         # F, L, Y
         counts = np.zeros((n_classes, n_classes, n_classes))
-        for xb, yb in testloader:
+        test_counts = np.zeros((n_classes, n_classes, n_classes))
+        
+        for xb, yb in trainloader:
             xb = xb.to(DEVICE)
             conv_predictions = torch.argmax(conv_model(xb).cpu(), dim=1)
             linear_predictions = torch.argmax(linear_model(xb).cpu(), dim=1)
             for i in range(len(yb)):
                 counts[conv_predictions[i], linear_predictions[i], yb[i]] += 1
+
+        for xb, yb in testloader:
+            xb = xb.to(DEVICE)
+            conv_predictions = torch.argmax(conv_model(xb).cpu(), dim=1)
+            linear_predictions = torch.argmax(linear_model(xb).cpu(), dim=1)
+            for i in range(len(yb)):
+                test_counts[conv_predictions[i], linear_predictions[i], yb[i]] += 1
             
 
-        probs = counts/len(testloader.dataset)
-        print(probs)
-
+        probs_train = counts/len(trainloader.dataset)
+        probs_test = test_counts/len(testloader.dataset)
+        mi_fy_train = mutual_information(probs_train, False)
+        mi_fy_test = mutual_information(probs_test, False)
+        mu = mi_fy_test - mutual_information(probs_test, True)
+        history_mi_train.append(mi_fy_train)
+        history_mi_test.append(mi_fy_test)
+        history_mu.append(mu)
+    
+    plt.plot(history_mi_train)
+    plt.plot(history_mi_test)
+    plt.plot(history_mu)
+    plt.legend(['MI train', 'MI test', 'mu'])
+    plt.show()
 
 if __name__ == "__main__":
     batch_size = 64
     lr=0.001
     trainloader, testloader, classes = get_data(batch_size=batch_size)
 
-    conv_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', num_classes=2, weights=None).to(DEVICE)
+    # conv_model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', num_classes=2, weights=None).to(DEVICE)
+    conv_model = torchvision.models.resnet18(num_classes=2)
+
+    # MNIST has 1 channel whereas regular ResNet expects 3
+    conv_model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+    conv_model.to(DEVICE)
     linear_model = LinearModel().to(DEVICE)
 
+
+
+    # print(summary(conv_model, (1, 244, 244)))
+
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(conv_model.parameters(), lr=lr)
-    fit(conv_model=conv_model, linear_model=linear_model, epochs=10, loss_func=criterion, trainloader=trainloader, testloader=testloader, optimizer=optimizer)
+    conv_optimizer = optim.SGD(conv_model.parameters(), lr=lr)
+    linear_optimizer = optim.SGD(linear_model.parameters(), lr=lr*10)
+    fit(conv_model=conv_model, linear_model=linear_model, epochs=10, loss_func=criterion, trainloader=trainloader, testloader=testloader, conv_optimizer=conv_optimizer, linear_optimizer=linear_optimizer)
 
 
     # # show some examples
